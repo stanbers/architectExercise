5abf879abb548d4a80036220d9d313c10c116fb9
stanxu <stanxu526@gmail.com>
2022-07-25 20:33:19 +0800

    ignore .class file

--- exercise-dev-api/src/main/java/com/stanxu/Application.java
+++ exercise-dev-api/src/main/java/com/stanxu/Application.java
@@ -19,6 +19,3 @@ public class Application {
 //        return String.format("Hello %s!",name);
 //    }
 }
-//2.3.4.RELEASE/spring-boot-autoconfigure-2.3.4.RELEASE.jar!/org/springframework/boot/autoconfigure/transaction/TransactionAutoConfiguration.class
-//2.3.4.RELEASE/spring-boot-autoconfigure-2.3.4.RELEASE-sources.jar!/org/springframework/boot/autoconfigure/transaction/TransactionAutoConfiguration.java
-//https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbXp2cFUzSjZWaHhxQTRZelpLb0lWaDhhVWJMUXxBQ3Jtc0tsYWNScE9QQ3pILXdSOURLSlkxVEFaOWg5cFhodU93SWxMN3l2c3hvbG1SQ3Z6Sm1sQWN6aXFGeEF2OHBXbGE4V0xpa1hYcWllM184dlVBdzdyVHpJVzE5bkw2V0kwMFpObGFWajRMc2EzQmZhN09RUQ&q=https%3A%2F%2Fwww.javainuse.com%2Fspring%2Fboot-transaction-propagation&v=zVRO-LELoSw
