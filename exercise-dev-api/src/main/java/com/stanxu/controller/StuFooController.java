1ac936a43abca0a3156cd55ba7efcf6be333e455
stanxu <stanxu526@gmail.com>
2022-07-30 17:54:38 +0800

    enable log4j, version 1.7

--- exercise-dev-api/src/main/java/com/stanxu/controller/StuFooController.java
+++ exercise-dev-api/src/main/java/com/stanxu/controller/StuFooController.java
@@ -2,19 +2,24 @@ package com.stanxu.controller;
 
 import com.stanxu.pojo.Stu;
 import com.stanxu.service.StuService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StuFooController {
 
+    final static private Logger logger = LoggerFactory.getLogger(StuFooController.class);
     @Autowired
     private StuService stuService;
 
     @GetMapping("/getStu")
-    public Object getStuInfo(int id){
+    public Object getStuInfo(@RequestParam int id){
+        logger.info("test log4j...");
         return stuService.getStuInfo(id);
     }
 
